ed16a884477602b5dead03a39c871b7958eb5855
rahulmunjal8 <rahulmunjal8@gmail.com>
2018-11-11 14:28:43 +0530

    jenkins demo

--- Rahul_Selenium/src/main/java/testcase/TC001_LoginAndLogOut.java
+++ Rahul_Selenium/src/main/java/testcase/TC001_LoginAndLogOut.java
@@ -1,23 +1,23 @@
-package testcase;
-
-
-
-import wdMethods.ProjectMethods;
-
-
-public class TC001_LoginAndLogOut extends ProjectMethods{
-	
-	
-	public void loginApp() {
-		loginApp();
-	}
-	
-	
-}
-
-
-
-
-
-
-
+//package testcase;
+//
+//
+//
+//import wdMethods.ProjectMethods;
+//
+//
+//public class TC001_LoginAndLogOut extends ProjectMethods{
+//	
+//	
+//	public void loginApp() {
+//		login();
+//	}
+//	
+//	
+//}
+//
+//
+//
+//
+//
+//
+//
